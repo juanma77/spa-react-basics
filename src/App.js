a8aaa71c999e42d18a94675711eb31b65f75f709
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-05-25 18:46:58 -0500

    using classes to define components and working with the state for first time

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './App.css';
 
-function HelloWorldComponent(props){
+/* function HelloWorldComponent(props){
   return (
   <div id="hello-world-component">
     <h3>{ props.subtitle }</h3>  
     <p>{ props.mytext }</p>
   </div> 
   );
+} */
+
+class HelloWorldComponent extends React.Component {
+
+  state = {
+    show: true
+  }
+
+  render(){
+    if(this.state.show){
+      return (
+        <div id="hello-world-component">
+          <h3>{ this.props.subtitle }</h3>  
+          <p>{ this.props.mytext }</p>
+          <button onClick={ () => this.setState({ show: false }) } type="button" class="btn btn-primary">Change State</button>
+        </div> 
+      );
+    }else{
+      return <h1>There are not elements!</h1>
+    } 
+  }
 }
 
 // Este componente App es el padre; aquí mandamos props o propiedades, en este en particular usamos "mytext" y "subtitle", y ya que la enviamos como propiedad desde el componente hijo que está incrustado en el padre, debemos de recibir esos props en el componente hijo
